76d82fa0da742984a5502022b4e3c9c2e0a3b2d6
fape <fape88@gmail.com>
2013-09-08 16:51:15 +0200

    Initial version of multiple series

--- data.php
+++ data.php
@@ -10,102 +10,131 @@ require_once('config.inc.php');
 
 // get the parameters
 
-$callback = $_GET['callback'];
-if (!preg_match('/^[a-zA-Z0-9_]+$/', $callback)) {
+$type = isset($_GET['type']) ? $_GET['type'] : null;
+
+if(!$type) {
+	die("invalid type");
+}
+
+$callback = isset($_GET['callback']) ? $_GET['callback'] : null;
+if (!$callback || !preg_match('/^[a-zA-Z0-9_]+$/', $callback)) {
 	die('Invalid callback name');
 }
 
-$sensor_id = isset($_GET['sensor'])? $_GET['sensor'] : null;
-if (!$sensor_id || !preg_match('/^[0-9]+$/', $sensor_id)) {
-	die("Invalid sensor parameter: $sensor_id");
-}
-
-$start = isset($_GET['start'])? $_GET['start'] : null;
-if ($start && !preg_match('/^[0-9]+$/', $start)) {
-	die("Invalid start parameter: $start");
-}
-
-$end = isset($_GET['end']) ? $_GET['end'] : null;
-if ($end && !preg_match('/^[0-9]+$/', $end)) {
-	die("Invalid end parameter: $end");
-}
-if (!$end) {
-	$end = time() * 1000;
+$unit_id = isset($_GET['unit'])? $_GET['unit'] : null;
+if (!$unit_id || !preg_match('/^[0-9]+$/', $unit_id)) {
+	die("Invalid unit parameter");
 }
 
 mysql_connect(DBHOST, DBUSER, DBPASSWORD) or die('die: mysql_connect');
 mysql_select_db(DBDATABASE) or die('die: mysql_select_db');
 
-// set UTC time
-//mysql_query("SET time_zone = '+00:00'");
+if ("data" === $type) {
+	$start = isset($_GET['start'])? $_GET['start'] : null;
+	if ($start && !preg_match('/^[0-9]+$/', $start)) {
+		die("Invalid start parameter: $start");
+	}
 
-// set some utility variables
-$range = $end - $start;
-$startTime = strftime('%Y-%m-%d %H:%M:%S', $start / 1000);
-//$endTime = gmstrftime('%Y-%m-%d %H:%M:%S', $end / 1000);
-$endTime = strftime('%Y-%m-%d %H:%M:%S', $end / 1000);
+	$end = isset($_GET['end']) ? $_GET['end'] : null;
+	if ($end && !preg_match('/^[0-9]+$/', $end)) {
+		die("Invalid end parameter: $end");
+	}
+	if (!$end) {
+		$end = time() * 1000;
+	}
 
-if( $range < 3 * 3600 * 1000 )
-{
-	$group_by="time";
-}
-elseif($range < 10 * 3600 * 1000)
-{
-	$group_by = " ROUND( UNIX_TIMESTAMP( TIME ) / ( 5 * 60 ) ) ";
-}
-// one day range loads 10 min data
-elseif ($range < 2 * 24 * 3600 * 1000) {
-	//$group_by = "time";
-	$group_by = " ROUND( UNIX_TIMESTAMP( TIME ) / ( 10 * 60 ) ) ";
-// one month range loads hourly data
-} elseif ($range < 31 * 24 * 3600 * 1000) {
-	#$group_by = "DATE_FORMAT(time , '%Y-%m-%d %H')";
-	$group_by = " ROUND( UNIX_TIMESTAMP( time ) / ( 24 * 60 * 60 ) )";
+	// set UTC time
+	//mysql_query("SET time_zone = '+00:00'");
 
-// one year range loads daily data
-} elseif ($range < 15 * 31 * 24 * 3600 * 1000) {
-	//$group_by = "DATE_FORMAT(time , '%Y-%m-%d')";
-	$group_by = " ROUND( UNIX_TIMESTAMP( time ) / ( 31 * 24 * 60 * 60 ) )";
+	// set some utility variables
+	$range = $end - $start;
+	$startTime = strftime('%Y-%m-%d %H:%M:%S', $start / 1000);
+	//$endTime = gmstrftime('%Y-%m-%d %H:%M:%S', $end / 1000);
+	$endTime = strftime('%Y-%m-%d %H:%M:%S', $end / 1000);
 
-// greater range loads monthly data
-} else {
-	//$group_by = "DATE_FORMAT(time , '%Y-%m')";
-	$group_by = "( YEAR(time) * 100 + MONTH(time) )";
-} 
+	if( $range < 3 * 3600 * 1000 )
+	{
+		$group_by="time";
+	}
+	elseif($range < 10 * 3600 * 1000)
+	{
+		$group_by = " ROUND( UNIX_TIMESTAMP( TIME ) / ( 5 * 60 ) ) ";
+	}
+	// one day range loads 10 min data
+	elseif ($range < 2 * 24 * 3600 * 1000) {
+		//$group_by = "time";
+		$group_by = " ROUND( UNIX_TIMESTAMP( TIME ) / ( 10 * 60 ) ) ";
+	// one month range loads hourly data
+	} elseif ($range < 31 * 24 * 3600 * 1000) {
+		#$group_by = "DATE_FORMAT(time , '%Y-%m-%d %H')";
+		$group_by = " ROUND( UNIX_TIMESTAMP( time ) / ( 24 * 60 * 60 ) )";
 
+	// one year range loads daily data
+	} elseif ($range < 15 * 31 * 24 * 3600 * 1000) {
+		//$group_by = "DATE_FORMAT(time , '%Y-%m-%d')";
+		$group_by = " ROUND( UNIX_TIMESTAMP( time ) / ( 31 * 24 * 60 * 60 ) )";
 
-$sql = "
-
-		select 
-			min(unix_timestamp(time) * 1000) as datetime,
-			avg(data) as data
-		from data 
-		where time between '$startTime' and '$endTime' 
-			and sensor_id = $sensor_id
-		group by $group_by
-		order by time
-		limit 0,5000
-
-
-";
-
-$result = mysql_query($sql) or die(mysql_error());
+	// greater range loads monthly data
+	} else {
+		//$group_by = "DATE_FORMAT(time , '%Y-%m')";
+		$group_by = "( YEAR(time) * 100 + MONTH(time) )";
+	}
 
 
-$rows = array();
-while ($row = mysql_fetch_assoc($result)) {
-	extract($row);
+	$sql = "
+
+			select 
+				data.sensor_id as id,
+				min(unix_timestamp(data.time) * 1000) as datetime,
+				avg(data.data) as data
+			from data 
+			INNER JOIN sensor ON sensor.id = data.sensor_id
+			where time between '$startTime' and '$endTime' 
+			and sensor.unit_id = $unit_id
+			group by $group_by, data.sensor_id
+			order by time
+			limit 0,5000
+
+
+	";
+
+	$result = mysql_query($sql) or die(mysql_error());
+
+	$datas=array();
+	while ($row = mysql_fetch_assoc($result)) {
+		extract($row);
+		$rows = array_key_exists($id, $datas) ? $datas[$id] : array(); 
+		$rows[] = array($datetime,$data);
+		$datas[$id] = $rows;
+	}
 	
-	$rows[] = "[$datetime,$data]";
+
+
+	// print it
+	header('Content-Type: text/javascript');
+	
+	echo "/* console.log(' start = $start, end = $end, startTime = $startTime, endTime = $endTime '); */\n";
+	echo $callback ."(".json_encode($datas, JSON_NUMERIC_CHECK ).");";
+} 
+else if ("sensor" === $type) {
+
+	$sql = "SELECT sensor.id, sensor.name as sensor, unit.name as unitname FROM sensor 
+				INNER JOIN unit ON sensor.unit_id = unit.id
+				WHERE unit.id = $unit_id";
+	
+	$result = mysql_query($sql) or die(mysql_error());
+
+
+	$rows = array();
+	while ($row = mysql_fetch_assoc($result)) {
+		extract($row);
+		
+		$rows[] = array($id, utf8_encode($sensor), utf8_encode($unitname));
+	}
+	// print it
+	header('Content-Type: text/javascript');
+	echo $callback ."(".json_encode($rows, JSON_NUMERIC_CHECK ).");";
 }
-
-// print it
-header('Content-Type: text/javascript');
-
-echo "/* console.log(' start = $start, end = $end, startTime = $startTime, endTime = $endTime '); */";
-echo $callback ."([\n" . join(",\n", $rows) ."\n]);";
-
-
 ?>
 
 
